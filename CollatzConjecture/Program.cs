8d9f6d2ba1a0fb4b902ef47304c1cf2ff95820ea
forageddev <emzyyt61@gmail.com>
2021-07-17 05:49:22 +0100

    made it not need that

--- CollatzConjecture/Program.cs
+++ CollatzConjecture/Program.cs
@@ -9,26 +9,24 @@ namespace CollatzConjecture {
             Console.WriteLine("Enter the number you wish to start with.");
 
             int startNumber = int.Parse(Console.ReadLine());
-            int number = startNumber, steps = 0, oddSteps = 0, eventSteps = 0;
+            int number = startNumber, oddSteps = 0, eventSteps = 0;
             string equation = startNumber + " ";
 
             while (number != 1) {
-                steps++;
                 if (number % 2 == 0) {
                     number = number / 2;
                     equation += "/ 2 = " + number + " ";
                     eventSteps++;
                 } else {
                     number = (number * 3) + 1;
-                    oddSteps++;
-
                     equation += "* 3 + 1 = " + number + " ";
+                    oddSteps++;
                 }
             }
 
             Console.WriteLine("---------------------------------------------");
             Console.WriteLine("Start Number: " + startNumber);
-            Console.WriteLine("Steps: " + steps);
+            Console.WriteLine("Steps: " + (oddSteps + eventSteps));
             Console.WriteLine("Odd Steps: " + oddSteps);
             Console.WriteLine("Even Steps: " + eventSteps);
             Console.WriteLine("---------------------------------------------");
